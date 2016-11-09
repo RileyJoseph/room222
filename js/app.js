0eb811ad2827632e867e614b04b07cd6585648e9
Riley Joseph <riley.m.joseph1@gmail.com>
2016-11-09 14:33:03 -0600

    removed public facing key

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ xhttp.onreadystatechange = function() {
        // Action to be performed when the document is read;
     }
 };
-var url = "https://api.sendgrid.com/v3/mail/send";
+// var url = "https://api.sendgrid.com/v3/mail/send";
 // function sendMail(){
 //     $.ajax(url, {
 //         type:"POST",
@@ -38,19 +38,28 @@ var url = "https://api.sendgrid.com/v3/mail/send";
 //     });
 // }
 
-function sendMail() {
-    var http = new XMLHttpRequest();
-    var data = {"personalizations": [{"to": [{"email": "riley.m.joseph1@gmail.com"}]}],"from": {"email": "example@example.com"},"subject": "Hello, World!","content": [{"type": "text/plain", "value": "Heya!"}]};
-    http.open("POST", url, true);
+// function sendMail() {
+//     var http = new XMLHttpRequest();
+//     var data = {"personalizations": [{"to": [{"email": "riley.m.joseph1@gmail.com"}]}],"from": {"email": "example@example.com"},"subject": "Hello, World!","content": [{"type": "text/plain", "value": "Heya!"}]};
+//     http.open("POST", url, true);
+//     var options = ("Access-Control-Request-Method","Access-Control-Request-Headers")
 
-    var options = ("Access-Control-Request-Method","Access-Control-Request-Headers")
+//     //Send the proper header information along with the request
+//     http.setRequestHeader("Content-type", "application/jsonp", "Access-Control-Allow-Origin", "*", "Authorization", "Bearer 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", "origin","http://localhost:8000");
 
-    //Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded", "Authorization", "Bearer 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", "origin","http://localhost:8000", "Access-Control-Allow-Origin");
-
-    http.send(data);
-}
-
+//     http.send(data);
+// }
+// var http = new XMLHttpRequest();
+// var body = {"personalizations": [{"to": [{"email": "riley.m.joseph1@gmail.com"}]}],"from": {"email": "example@example.com"},"subject": "Hello, World!","content": [{"type": "text/plain", "value": "Heya!"}]};
+// function callOtherDomain(){
+//     if(http) {
+//         http.open('POST', url, true);
+//         http.setRequestHeader('X-PINGOTHER', 'pingpong');
+//         http.setRequestHeader('Content-Type', 'application/xml');
+//         // http.onreadystatechange = handler;
+//         http.send(body);
+//     }
+// }
 // function sendMail2(){ 
 //     var request = new XMLHttpRequest();
 //     var params = {"personalizations": [{"to": [{"email": "riley.m.joseph1@gmail.com"}]}],"from": {"email": "example@example.com"},"subject": "Hello, World!","content": [{"type": "text/plain", "value": "Heya!"}]};
@@ -81,4 +90,40 @@ function sendMail() {
 // }
 // xhttp.open("GET", "filename", true);
 // xhttp.send();
+var url = "https://api.sendgrid.com/v3/templates/"
+function getMail(){   
+    var xhr = createCORSRequest('GET', url);
+    xhr.send();
+}
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  if ("withCredentials" in xhr) {
+    // Access-Control-Allow-Credentials: true
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+var xhr = createCORSRequest('GET', url);
+if (!xhr) {
+  throw new Error('CORS not supported');
+}
 
